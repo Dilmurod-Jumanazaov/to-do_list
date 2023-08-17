e07e49e0d4d4c164722423464d4b274e28e9ce4d
Dilmurod-Jumanazarov <jumanazarovdilmurod0002@gmail.com>
2023-08-17 09:30:09 +0500

    Add to html file btn

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
-var elForm = document.querySelector(".form-box");
-var elInput = elForm.querySelector(".form-box__input");
-var elTimeInput = elForm.querySelector(".form-box__time-input");
-var elList = document.querySelector(".list");
+const elForm = document.querySelector(".form-box");
+const elInput = elForm.querySelector(".form-box__input");
+const elTimeInput = elForm.querySelector(".form-box__time-input");
+const elVoiceBtn = document.querySelector(".voice__btn");
+const elList = document.querySelector(".list");
+const elVoiceText = document.querySelector(".voice-text");
+
 // Bu yerda array yaratib oldik.Va uning ichida object datalari saqladik.
-var newArray = [
+let newArray = [
   {
     "title": "Ertalab turaman",
     "time": "06:00"
@@ -23,22 +26,6 @@ var newArray = [
   {
     "title": "Tushlik qilaman",
     "time": "12:00"
-  },
-  {
-    "title": "Ertalab turaman",
-    "time": "06:00"
-  },
-  {
-    "title": "Dushga tushaman",
-    "time": "13:00"
-  },
-  {
-    "title": "O'qishga chiqib ketaman",
-    "time": "13:30"
-  },
-  {
-    "title": "Darsim boshlanadi",
-    "time": "15:30"
   }
 ];
 
@@ -48,9 +35,9 @@ function toDo(todoList) {
   elList.innerHTML = "";
   todoList.forEach(function (item,index,arr) {
     // bu yerda HTMLda yangi li,p,time elementlarini yaratib oldik
-    var liElement = document.createElement("li");
-    var titleElement = document.createElement("p");
-    var timeElement = document.createElement("time");
+    let liElement = document.createElement("li");
+    let titleElement = document.createElement("p");
+    let timeElement = document.createElement("time");
     // bu yerda timeElemnti uchun attribut qoshdik. "setAttribute" methodi yordamida.
     timeElement.setAttribute("datetime","2002-07-27");
     // bu yerda create qilgan elementlarimizga css dagi classlarni qoshib qodik.
@@ -70,17 +57,62 @@ toDo(newArray);
 elForm.addEventListener("submit", function(evt) {
   evt.preventDefault();;
   // bu yerda inputdan kelyotgan valuelarni oldik
-  var inputValue = elInput.value.trim();
-  var timeInput = elTimeInput.value;
+  let inputValue = elInput.value.trim();
+  let timeInput = elTimeInput.value;
   // bu yerda bitta new object yaratdik.Va uning ichiga inputdan kelyotgan datalarni joylab qoyyabmiz.
-  var new_obj = {
+  let new_obj = {
     "title": inputValue,
     "time": timeInput
   }
   // bu yerda bitta new array yaratdik va uning ichiga yaratilgan new_obj ni ".push" methodi yordamida joylab qo'yyabmiz.
   newArray.unshift(new_obj);
-
+  
   toDo(newArray);
   
 })
 
+// let date = new Date(); // build in class yani obj (obyekt);
+// let hour = date.getHours();
+// let minute = date.getMinutes();
+
+// let voiceRecord = new webkitSpeechRecognition();
+// // voiceRecord.lang = "";
+
+// voiceRecord.onresult = function(evt) {
+//   let title = evt.results[0][0].transcript;
+//   let voice_obj = {
+//     "title": title,
+//     "time": `${hour}:${minute}`
+//   }
+//   newArray.push(voice_obj);
+//   toDo(newArray);
+// }
+
+// elVoiceBtn.addEventListener("click", function() {
+//   voiceRecord.start();
+//   elVoiceText.classList.add("d-block");
+// });
+// voiceRecord.onend = function() {
+//   elVoiceText.classList.add("voice-text");
+// }
+
+let date = new Date() // new Date bu JS ni ozida bolgan build in class yani ozi ham bitta obj(obyekt)
+let hour = date.getHours();
+let minute = date.getMinutes();
+
+const voiceRecord = new webkitSpeechRecognition();
+
+voiceRecord.onresult = function(evt) {
+  let title = evt.results[0][0].transcript;
+  let voice_obj = {
+    "title": title,
+    "time": `${hour}:${minute + 1}`
+  }
+  newArray.push(voice_obj);
+  toDo(newArray);
+}
+
+elVoiceBtn.addEventListener("click", function() {
+  voiceRecord.start();
+  elVoiceText.classList.add("d-block");
+})
